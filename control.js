bfb7611418b20fdf8bde1feddd8904507b586d67
benjamin <louis.benjamin@live.fr>
2024-08-03 09:48:07 +0200

    début

---
+++ control.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const axios = require('axios'); 
+
+const controlLight = async(lightID,on){
+    try{
+        return await axios.put(
+            `http://${process.env.HUE_BRIGE_IP}/api/${process.env.HUE_USERNAME}/lights/${lightID}:state`,
+            {on});       
+        }catch(err){
+            console.error(error)
+        }
+}
+
+controlLight(6,true);
